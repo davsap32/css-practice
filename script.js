8fb930882c9230097f13eb8dc8cbfe716b10f157
davsap32 <69012937+davsap32@users.noreply.github.com>
2022-08-30 07:25:51 +0200

    change color of clock text

--- script.js
+++ script.js
@@ -2,18 +2,21 @@ const contentLarge = document.querySelector(".content-large");
 const contentClock = document.querySelector(".clock");
 const contentSmall1 = document.querySelector(".content-small-1");
 const showClock = function(){
-	contentClock.textContent = new Intl.DateTimeFormat("en-SE", {
-    year: "numeric",
-    month: "numeric",
-    day: "numeric",
-    hour: "numeric",
-    minute: "numeric",
-    second: "numeric",
-}).format(new Date());
+		contentClock.textContent = new Intl.DateTimeFormat("en-SE", {
+	    year: "numeric",
+	    month: "numeric",
+	    day: "numeric",
+	    hour: "numeric",
+	    minute: "numeric",
+	    second: "numeric",
+	}).format(new Date());
+	contentClock.style.color = 'green';
 }
 showClock();
 setInterval(function () {
 	showClock();
+	if(contentClock.style.color === 'green')contentClock.style.color = 'red'
+	else contentClock.style.color = 'green'
 }, 1000);
 const arr = [];
 for (let i = 0; i < 25; i++) {
