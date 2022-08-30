a1dc1cb3f41bb782cd41ac8487aa78b53a2a2b51
davsap32 <69012937+davsap32@users.noreply.github.com>
2022-08-30 10:05:31 +0200

    simplified change of color

--- script.js
+++ script.js
@@ -1,30 +1,23 @@
 const contentLarge = document.querySelector(".content-large");
 const contentClock = document.querySelector(".clock");
 const contentSmall1 = document.querySelector(".content-small-1");
-const showClock = function(){
-		contentClock.textContent = new Intl.DateTimeFormat("en-SE", {
-	    year: "numeric",
-	    month: "numeric",
-	    day: "numeric",
-	    hour: "numeric",
-	    minute: "numeric",
-	    second: "numeric",
-	}).format(new Date());
-	//contentClock.style.color = 'green';
-}
+const showClock = function () {
+  contentClock.textContent = new Intl.DateTimeFormat("en-SE", {
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+  }).format(new Date());
+  //contentClock.style.color = 'green';
+};
 showClock();
-let flag=0;
+let color;
 setInterval(function () {
-	showClock();
-	if(!flag){
-		contentClock.style.color = 'red';
-		flag = 1;
-	}
-	else {
-		contentClock.style.color = 'green'
-		flag = 0;
-	}
-	
+  showClock();
+  color = color !== "green" ? "green" : "red";
+  contentClock.style.color = color;
 }, 1000);
 const arr = [];
 for (let i = 0; i < 25; i++) {
